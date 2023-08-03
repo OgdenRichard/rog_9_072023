357d0fc214a0dbe8937451868a04876234fa1f2a
Richard Ogden <ogden.richard@gmail.com>
2023-08-03 16:54:06 +0200

    03082023 | NewBill.js | basic test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,18 +1,45 @@
 /**
  * @jest-environment jsdom
  */
+import "@testing-library/jest-dom";
+import { screen, waitFor } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import { ROUTES } from "../constants/routes";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import userEvent from "@testing-library/user-event";
+import mockStore from "../__mocks__/store";
+import router from "../app/Router.js";
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+jest.mock("../app/store", () => mockStore);
 
 describe("Given I am connected as an employee", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "localStorage", {
+      value: localStorageMock,
+    });
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        type: "Employee",
+      })
+    );
+  });
+
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    test("Then the New Bill icon in vertical layout should be highlighted", async () => {
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+      await waitFor(() => screen.getByTestId("icon-mail"));
+      const newBillIcon = screen.getByTestId("icon-mail");
+      expect(newBillIcon).toHaveClass("active-icon");
+      await waitFor(() => screen.getByTestId("icon-window"));
+      const billsIcon = screen.getByTestId("icon-window");
+      expect(billsIcon).not.toHaveClass("active-icon");
+    });
+  });
+});

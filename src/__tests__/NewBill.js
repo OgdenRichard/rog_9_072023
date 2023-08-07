eb76637afeb6da1b6a621ceedce5227f0404fbf0
Richard Ogden <ogden.richard@gmail.com>
2023-08-07 16:31:01 +0200

    08082023 | NewBill.js > tests integration formulaire

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -28,22 +28,82 @@ describe("Given I am connected as an employee", () => {
   });
 
   describe("When I am on NewBill Page", () => {
-    test("Then the New Bill icon in vertical layout should be highlighted", async () => {
+    beforeAll(() => {
       const root = document.createElement("div");
       root.setAttribute("id", "root");
       document.body.append(root);
       router();
       window.onNavigate(ROUTES_PATH.NewBill);
+    });
+
+    test("Then the New Bill icon in vertical layout should be highlighted", async () => {
       await waitFor(() => screen.getByTestId("icon-mail"));
       const newBillIcon = screen.getByTestId("icon-mail");
       expect(newBillIcon).toHaveClass("active-icon");
       await waitFor(() => screen.getByTestId("icon-window"));
       const billsIcon = screen.getByTestId("icon-window");
+      //
       expect(billsIcon).not.toHaveClass("active-icon");
+      await waitFor(() => {
+        screen.getAllByText("Envoyer une note de frais");
+      });
     });
 
-    test('',() => {
-      
-    })
+    test("Then the NewBill page displays a title and a form", async () => {
+      const nf = screen.getAllByText("Envoyer une note de frais");
+      expect(nf).toBeTruthy();
+      await waitFor(() => screen.getByTestId("form-new-bill"));
+      const newBillIcon = screen.getByTestId("form-new-bill");
+      expect(newBillIcon).toBeTruthy();
+    });
+
+    test("Then the form displays a field for expense type which is required", async () => {
+      await waitFor(() => screen.getByTestId("expense-type"));
+      const expenseType = screen.getByTestId("expense-type");
+      expect(expenseType).toBeTruthy();
+      expect(expenseType.hasAttribute("required")).toBe(true);
+    });
+
+    test("Then the form displays a field for expense name which is optional", async () => {
+      await waitFor(() => screen.getByTestId("expense-name"));
+      const expenseName = screen.getByTestId("expense-name");
+      expect(expenseName).toBeTruthy();
+      expect(expenseName.hasAttribute("required")).toBe(false);
+    });
+
+    test("Then the form displays a datepicker input which is required", async () => {
+      await waitFor(() => screen.getByTestId("datepicker"));
+      const datePicker = screen.getByTestId("datepicker");
+      expect(datePicker).toBeTruthy();
+      expect(datePicker.hasAttribute("required")).toBe(true);
+    });
+
+    test("Then the form displays a VAT amount field which is optional", async () => {
+      await waitFor(() => screen.getByTestId("vat"));
+      const vatAmountField = screen.getByTestId("vat");
+      expect(vatAmountField).toBeTruthy();
+      expect(vatAmountField.hasAttribute("required")).toBe(false);
+    });
+
+    test("Then the form displays a VAT percentage field which is required", async () => {
+      await waitFor(() => screen.getByTestId("pct"));
+      const vatPercentageField = screen.getByTestId("pct");
+      expect(vatPercentageField).toBeTruthy();
+      expect(vatPercentageField.hasAttribute("required")).toBe(true);
+    });
+
+    test("Then the form displays a comment field which is optional", async () => {
+      await waitFor(() => screen.getByTestId("commentary"));
+      const comments = screen.getByTestId("commentary");
+      expect(comments).toBeTruthy();
+      expect(comments.hasAttribute("required")).toBe(false);
+    });
+
+    test("Then the form displays a file input field which is required", async () => {
+      await waitFor(() => screen.getByTestId("file"));
+      const fileInput = screen.getByTestId("file");
+      expect(fileInput).toBeTruthy();
+      expect(fileInput.hasAttribute("required")).toBe(true);
+    });
   });
 });
